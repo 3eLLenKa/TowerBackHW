8a14a4951ea1fbb51d8bba80be40c52a9e10e05c
3eLLenKa <alexkomkov39@gmail.com>
2025-10-22 23:12:51 +0300

    part 2: done

--- bashnya-hw2/main.go
+++ bashnya-hw2/main.go
@@ -5,12 +5,87 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var (
 	ErrInvalidNum   = errors.New("invalid num")
 	ErrServiceError = errors.New("service error")
 )
+
+func ToString(num int) string {
+	if num == 0 {
+		return "ноль"
+	}
+
+	if num < 0 || num > 99999 {
+		return "число вне диапазона"
+	}
+
+	units := []string{"", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять",
+		"десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать",
+		"шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"}
+
+	tens := []string{"", "", "двадцать", "тридцать", "сорок", "пятьдесят",
+		"шестьдесят", "семьдесят", "восемьдесят", "девяносто"}
+
+	hundreds := []string{"", "сто", "двести", "триста", "четыреста", "пятьсот",
+		"шестьсот", "семьсот", "восемьсот", "девятьсот"}
+
+	result := ""
+
+	// тысячи
+	if num >= 1000 {
+		thousands := num / 1000
+		if thousands == 1 {
+			result += "одна тысяча "
+		} else if thousands == 2 {
+			result += "две тысячи "
+		} else {
+			// от 3 до 19
+			if thousands < 20 {
+				result += units[thousands] + " "
+			} else {
+				// от 20 до 99
+				result += tens[thousands/10] + " "
+				if thousands%10 > 0 {
+					result += units[thousands%10] + " "
+				}
+			}
+			// определяем окончание для тысяч
+			if thousands >= 5 && thousands <= 19 {
+				result += "тысяч "
+			} else if thousands%10 == 1 {
+				result += "тысяча "
+			} else if thousands%10 >= 2 && thousands%10 <= 4 {
+				result += "тысячи "
+			} else {
+				result += "тысяч "
+			}
+		}
+
+		num %= 1000
+	}
+
+	// сотни
+	if num >= 100 {
+		result += hundreds[num/100] + " "
+		num %= 100
+	}
+
+	// десятки и единицы
+	if num >= 20 {
+		result += tens[num/10] + " "
+		num %= 10
+	}
+
+	// единицы и числа 10-19
+	if num > 0 {
+		result += units[num]
+	}
+
+	return strings.TrimSpace(result)
+}
 
 func Task2(num int) (int, error) {
 	if num >= 12307 {
@@ -52,5 +127,5 @@ func main() {
 		return
 	}
 
-	fmt.Printf("\nРезультат программы: %d", res)
+	fmt.Printf("\nРезультат программы: %d (%s)", res, ToString(res))
 }
